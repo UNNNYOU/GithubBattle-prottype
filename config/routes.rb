38c05b1ff3536e519e2868802196ea05eb104a52
UNNNYOU <unnnyou@gmail.com>
2024-04-30 17:02:30 +0900

    ログイン時にユーザー一覧画面へ遷移していなかったためroutes.rbの編集を行いました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'top#index'
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   resources :users, only: %i[index show]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
